a9a4e8eb816c5ed36e4c1605c14d8cc168d9a23c
washedDeveloper <patrick.brophy2@gmail.com>
2020-05-02 17:06:12 -0400

    fixed server folder

--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const api = require('./routes/api');
 const app = express();
 
 const PORT = 8081 || process.env.PORT;
@@ -7,6 +7,8 @@ const PORT = 8081 || process.env.PORT;
 app.get('/', (req,res) => {
     res.json({message: "Hello Express"});
 });
+
+app.use('/api', api);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
