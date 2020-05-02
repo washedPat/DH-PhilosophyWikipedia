537e60c8e59b2252391d32e182009cffbca211f4
washedDeveloper <patrick.brophy2@gmail.com>
2020-05-02 17:21:12 -0400

    added static

--- server/index.js
+++ server/index.js
@@ -2,7 +2,9 @@ const express = require('express');
 const api = require('./routes/api');
 const app = express();
 
-const PORT = 8081 || process.env.PORT;
+const PORT = 8080 || process.env.PORT;
+
+app.use(express.static('public'));
 
 app.get('/', (req,res) => {
     res.json({message: "Hello Express"});
